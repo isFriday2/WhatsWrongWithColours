0cef4db7b57901c08d5c73c02b5cdf4f2cd5ab49
Sonnie Poon <n10929436@qut.edu.au>
2024-10-07 11:20:09 +1000

    [NEW & BUG FIX] Overlays Switching in 5 second interval, fixed screenwidth undefine

---
+++ dist/sketch.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var toggle = true;
+
+function preload() {
+  bg = loadImage('/assets/illustration.PNG');
+  textOverlay1 = loadImage('/assets/textOverlay-01.PNG');
+  textOverlay2 = loadImage('/assets/textOverlay-02.PNG');
+}
+
+function setup() {
+  createCanvas(screeenWidth, screenHeight);
+}
+
+function draw() {
+  background("#fff");
+  image(bg, 0, 0, screenWidth, screenHeight / 2.86);
+
+  if (toggle) {
+    image(textOverlay1, 0, 0, screenWidth, screenHeight / 2.86);
+  } else image(textOverlay2, 0, 0, screenWidth, screenHeight / 2.86);
+}
+
+function keyPressed(e) {
+  console.log(mouseX, mouseY);
+  toggle = !toggle;
+}
--- sketch.js
+++ sketch.js
@@ -1,23 +1,53 @@
 var toggle = true;
+
+var lightOverlays = [];
+var imageToDisplay = 0;
+
+let lastChangeTime = 0;
+const changeInterval = 5000;
+
 function preload(){
   bg = loadImage('/assets/illustration.PNG');
   textOverlay1 = loadImage('/assets/textOverlay-01.PNG')
   textOverlay2 = loadImage('/assets/textOverlay-02.PNG')
- 
+
+  // overlays
+  lightOverlays.push(loadImage("assets/overlay/alexa-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/abu-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/frank-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/larena-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/moo-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/reya-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/rosie-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/sity-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/strawberryMan-dark.png"))
+  lightOverlays.push(loadImage("assets/overlay/xiaoming-dark.png"))
 }
 
 function setup() {
-  createCanvas(screeenWidth, screenHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background("#fff");
-  image(bg, 0, 0, screenWidth, screenHeight/2.86);
+  image(bg, 0, 0, windowWidth, windowHeight/2.3);
   
+  
+
   if(toggle){
-    image(textOverlay1, 0, 0, screenWidth, screenHeight/2.86);
-  }else image(textOverlay2, 0, 0, screenWidth, screenHeight/2.86);
-  
+    image(textOverlay1, 0, 0, windowWidth, windowHeight/2.3);
+  }else image(textOverlay2, 0, 0, windowWidth, windowHeight/2.3);
+
+
+  if (millis() - lastChangeTime > changeInterval) {
+    imageToDisplay+=1 
+    if (imageToDisplay >= lightOverlays.length) imageToDisplay = 0
+
+    lastChangeTime = millis(); // Reset the timer
+  }
+
+  tint(255, 200);
+  image(lightOverlays[imageToDisplay], 0, 0, windowWidth, windowHeight/2.3)
   
 }
 function keyPressed(e){
